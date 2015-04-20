2ab4d86b12dc9dbf3dc3ea62f3c559c706379f9e
Rüdiger zu Dohna <snackbox@sinntr.eu>
2015-04-20 17:50:38 +0200

    tutorial: get pojo

--- src/test/java/com/github/t1/rest/TutorialTest.java
+++ src/test/java/com/github/t1/rest/TutorialTest.java
@@ -1,6 +1,7 @@
 package com.github.t1.rest;
 
 import static org.junit.Assert.*;
+import lombok.Data;
 
 import org.junit.*;
 
@@ -8,15 +9,29 @@ public class TutorialTest {
     @Rule
     public final RestClientMock service = new RestClientMock();
 
+    @Data
+    public static class Pojo {
+        String key, value;
+    }
+
     @Before
     public void before() {
         service.on("http://example.org/path").reply("value");
+        service.on("http://example.org/pojo").reply("{\"key\":\"k\",\"value\":\"v\"}");
     }
 
     @Test
-    public void t1() {
+    public void testGetString() {
         String value = new RestResource("http://example.org/path").get(String.class);
 
         assertEquals("value", value);
+    }
+
+    @Test
+    public void testGetPojo() {
+        Pojo value = new RestResource("http://example.org/pojo").get(Pojo.class);
+
+        assertEquals("k", value.getKey());
+        assertEquals("v", value.getValue());
     }
 }
